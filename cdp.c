ff4c5927f56c01d8a9180bd54a098eafbfb6a35f
Richard Mitic <richard.h.mitic@gmail.com>
2017-03-05 00:17:52 +0100

    Move cdpprograms functions back into main file and remove the helper files

--- cdp.c
+++ cdp.c
@@ -4,7 +4,7 @@
 #include "ext_systhread.h"
 #include "cdp.h"
 #include "cdptask.h"
-#include "cdpprogram.h"
+#include "process_osx.h"
 
 
 void cdp_anything(t_cdp *x, t_symbol *s, long ac, t_atom *av);
@@ -19,7 +19,10 @@ void cdp_assist(t_cdp *x, void *b, long m, long a, char *s);
 void cdp_free(t_cdp *x);
 void *cdp_new(t_symbol *s, long ac, t_atom *av);
 
-
+t_max_err cdp_find_excutable(t_cdp *x, char *name, short *path, char *full_path);
+t_max_err cdp_make_cmd(t_cdp *x, char *executable, long ac, t_atom *av, t_string **cmd);
+void cdp_do_program(t_cdp *x, char *program, long ac, t_atom *av);
+char *get_filename_ext(char *filename);
 
 t_class *cdp_class;
 
@@ -35,7 +38,7 @@ void ext_main(void *r)
   class_addmethod(c, (method)cdp_anything,     "anything",     A_GIMME, 0);
   
 	// methods which we use internally. they all have A_GIMME style signature
-  class_addmethod(c, (method)cdp_docdp,    "docpd",    A_GIMME, 0);
+  class_addmethod(c, (method)cdp_docdp,        "docpd",        A_GIMME, 0);
 	class_addmethod(c, (method)cdp_taskcomplete, "taskcomplete", A_GIMME, 0);
 	class_addmethod(c, (method)cdp_taskoutput,   "taskoutput",   A_GIMME, 0);
 
@@ -194,6 +197,114 @@ void *cdp_new(t_symbol *s, long ac, t_atom *av)
 }
 
 
+
+///////////////////////////////////////////
+//  Helper methods to run a CDP process  //
+///////////////////////////////////////////
+
+t_max_err cdp_find_excutable(t_cdp *x, char *name, short *path, char *full_path)
+{
+  char tmp[MAX_PATH_CHARS];
+  t_fourcc type = 0;
+  t_max_err err = 0;
+
+  strcpy(tmp, x->cdp_path->s_name);
+  
+  // TODO: Simplify this logic
+  
+  // If user has specifed a location, look for it and use it if it exists.
+  // Else, look in the Max search path.
+  if (x->cdp_path && x->cdp_path != gensym("") && !locatefile_extended(tmp, path, &type, NULL, 0)) {
+    // User has defined a CDP root and it exists. Now check for executable.
+    err = path_topathname(*path, name, full_path);
+  } else {
+    // No CPD root defined. Check Max's search path.
+    err = locatefile_extended(name, path, &type, NULL, 0);
+    if (!err) {
+      err = path_topathname(*path, name, full_path);
+    }
+  }
+  
+  if (!err) {
+    strcpy(tmp, full_path);
+    path_nameconform(tmp, full_path, PATH_STYLE_NATIVE, PATH_TYPE_BOOT);
+  }
+  
+  return err;
+}
+
+
+t_max_err cdp_make_cmd(t_cdp *x, char *executable, long ac, t_atom *av, t_string **cmd)
+{
+  long argstrsize = 0;
+  char *argstr = NULL;
+  t_string *cmdstr = NULL;
+  
+  cmdstr = string_new(executable);
+  if (!cmdstr) {
+    object_error((t_object*)x, "could not create command string for %s", executable);
+    return 1;
+  }
+  
+  string_append(cmdstr, " ");
+  
+  atom_gettext(ac, av, &argstrsize, &argstr, 0);
+  if (!argstr) {
+    object_error((t_object*)x, "could not get arguments for %s", string_getptr(cmdstr));
+    return 2;
+  }
+  
+  string_append(cmdstr, argstr);
+  
+  *cmd = cmdstr;
+  
+  if (argstr)
+    sysmem_freeptr(argstr);
+  
+  return 0;
+}
+
+
+void cdp_do_program(t_cdp *x, char *program, long ac, t_atom *av)
+{
+  t_string *cmd = NULL;
+  char process_output[PROCESS_OUTPUT_MAX_SIZE];
+  int process_return;
+  t_atom av_out[2];
+  
+  short path = 0;
+  char fullpath[MAX_PATH_CHARS];
+  
+  if (cdp_find_excutable(x, program, &path, fullpath)) {
+    object_error((t_object*)x, "could not find excutable %s", program);
+    return;
+  } else {
+    post("found excutable %d %s", path, fullpath);
+  }
+  
+  // Final 2 atoms are for the return code and stdout.
+  if (cdp_make_cmd(x, fullpath, ac, av, &cmd)) {
+    return;
+  }
+  
+  process_return = run_process((char*)string_getptr(cmd), process_output, PROCESS_OUTPUT_MAX_SIZE);
+  
+  atom_setlong(av_out, process_return);
+  atom_setsym(av_out+1, gensym(process_output));
+  
+  defer_low(x, (method)cdp_taskoutput, gensym("cdpout"), 2, av_out);
+  
+  if (cmd)
+    object_free(cmd);
+}
+
+
+char *get_filename_ext(char *filename)
+{
+  char *dot = strrchr(filename, '.');
+  if(!dot || dot == filename) return "";
+  return dot + 1;
+}
 
 
 
